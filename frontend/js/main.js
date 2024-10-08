5680ae4fcbb3c2fa70732530ab5cb61eb35ccc85
mLascurain <matiaslascurain04@gmail.com>
2024-10-08 20:34:39 -0300

    vite app default settings

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,10 +1,10 @@
-import './css/style.css'
+import '../css/style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Hello Vite!</h1>
     <p class="read-the-docs">
-     sape 
+     sape
     </p>
   </div>
 `
