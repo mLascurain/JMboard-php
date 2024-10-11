aed8daa208bb2a065e11f6f4f1598e96bdc2bcfe
Matias Lascurain <matiaslascurain04@gmail.com>
2024-10-10 22:45:36 -0300

    added first styles

--- frontend/formulario_login.php
+++ frontend/formulario_login.php
@@ -11,27 +11,29 @@
 <body>
     <?php include("layout/header.php"); ?>
     <article class="article-form-login">
-        <h2 class="title-form">Iniciar Sesion</h2>
         <div>
-            <a href="../frontend/index.php">
-                <button class="btn-inicio">Inicio</button>
-            </a>
+            <h2 class="title-form">Iniciar Sesion</h2>
+            <div>
+                <a href="../frontend/index.php">
+                    <button class="btn-inicio">Inicio</button>
+                </a>
+            </div>
+            <form action="../backend/login.php" method="post" class="form-login">
+                <label>E-mail
+                    <input name="email" type="email" maxlength="30" />
+                </label>
+                <label>Contraseña
+                    <input type="password" name="password" maxlength="12" />
+                </label>
+                <input class="btn-login" type="submit" value="Login"/>
+                <a class="register" href="formulario_signup.php">Registrarse</a>	
+            </form>
+            
+            <?php if (isset($_SESSION['error'])): ?>
+                <p style="color: red;"> <?php echo $_SESSION['error']; ?> </p>
+                <?php unset($_SESSION['error']); ?>
+            <?php endif; ?>
         </div>
-        <form action="../backend/login.php" method="post" class="form-login">
-            <label>E-mail
-        	    <input name="email" type="email" maxlength="30" />
-            </label>
-            <label>Contraseña
-        	    <input type="password" name="password" maxlength="12" />
-            </label>
-            <input class="btn-login" type="submit" value="Login"/>
-            <a href="formulario_signup.php">Registrarse</a>	
-        </form>
-        
-        <?php if (isset($_SESSION['error'])): ?>
-            <p style="color: red;"> <?php echo $_SESSION['error']; ?> </p>
-            <?php unset($_SESSION['error']); ?>
-        <?php endif; ?>
 
     </article>
     <?php include("layout/footer.php"); ?>
--- frontend/layout/index_menu.php
+++ frontend/layout/index_menu.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>JMboard</title>
 </head>
 <body>
-    <P>holas</P>
+    <section class="principal">
+        <article>
+            <h1>JMboard</h1>
+            <p>La mejor herramienta open source para gestionar tus proyectos</p>
+            <span class="principal__buttons">
+                <a href="../frontend/formulario_login.php" target="_blank"><button class="article__button-wn">Iniciar experiencia</button></a>
+            </span>
+        </article>
+        <div>
+            <img src="imagenes/principal.jpg" alt="imagen principal del sitio">
+        </div>
+    </section>
 </body>
 </html>
