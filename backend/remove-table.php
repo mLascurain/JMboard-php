c6f0fc69baee64bdd5365803054d2c0d88c933b2
mLascurain <matiaslascurain04@gmail.com>
2024-10-11 22:27:41 -0300

    fixed a bug that occurred when you deleted a board

--- backend/remove-table.php
+++ backend/remove-table.php
@@ -1,8 +1,42 @@
 <?php session_start();?>
 <?php
 include("../backend/conexion.php");
-$id = $_POST['id'];
-$consulta = mysqli_query($conexion, "DELETE FROM tableros WHERE id = $id");
-header("location:../frontend/index.php");
-mysqli_free_result($consulta);
+$id_tablero = $_POST['id'];
+$consulta = "SELECT COUNT(*) AS num_columnas FROM columnas WHERE tablero_id = '$id_tablero'";
+$resultado = mysqli_query($conexion, $consulta);
+$num_columnas = mysqli_fetch_assoc($resultado)['num_columnas'];
+mysqli_free_result($resultado);
+
+for ($i = 0; $i < $num_columnas; $i++) { 
+    // Hay columnas asociadas al tablero, borrarlas también
+    $consultaColumnas = "SELECT id FROM columnas WHERE tablero_id = '$id_tablero'";
+    $resultadoColumnas = mysqli_query($conexion, $consultaColumnas);
+    $columna = mysqli_fetch_assoc($resultadoColumnas);
+    $id_columna = $columna['id'];
+    mysqli_free_result($resultadoColumnas);
+
+    // Comprobar si la columna tiene tareas
+    $consultaTareas = "SELECT COUNT(*) AS num_tareas FROM tareas WHERE columna_id = '$id_columna'";
+    $resultadoTareas = mysqli_query($conexion, $consultaTareas);
+    $num_tareas = mysqli_fetch_assoc($resultadoTareas)['num_tareas'];
+    mysqli_free_result($resultadoTareas);
+    for ($j = 0; $j < $num_tareas; $j++) {
+        // La columna tiene tareas, borrarlas también
+        $consultaBorrarTareas = "DELETE FROM tareas WHERE columna_id = '$id_columna'";
+        mysqli_query($conexion, $consultaBorrarTareas);
+    }
+
+    // Borrar la columna
+    $consultaBorrarColumna = "DELETE FROM columnas WHERE id = '$id_columna'";
+    mysqli_query($conexion, $consultaBorrarColumna);
+}
+
+// Borrar el tablero
+$consulta = "DELETE FROM tableros WHERE id = '$id_tablero'";
+mysqli_query($conexion, $consulta);
+
+// Redirigir a index.php
+header("Location: ../frontend/index.php");
+mysqli_close($conexion); // Cierra la conexión con la base de datos
+exit;
 ?>
