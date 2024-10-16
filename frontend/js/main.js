79fce031bd600d12b0b22d93c73688d45506b96e
Krilzs <jerofc143@gmail.com>
2024-10-16 15:20:42 -0300

    Modal for edit task was added

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,11 +1,28 @@
-import '../css/style.css'
+const $tareas = document.querySelectorAll(".tarea");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-    <p class="read-the-docs">
-     sape
-    </p>
-  </div>
-`
+function abrirTarea(index) {
+  setTimeout(function () {
+    const $modal = document.getElementById(`edit-task-${index}`); // Ajuste en el índice
+    if ($modal) {
+      $modal.classList.toggle("-show");
+    }
+  }, 50);
+}
 
+function enviarForm(index) {
+  const $form = document.getElementById(`edit-title-task-${index}`);
+  $form.submit();
+}
+
+function editarTitulo(index) {
+  const $title = document.getElementById(`title-task-${index}`);
+  const $title_input = document.getElementById(`input-title-task-${index}`); // Ajuste en el índice
+  if ($title) {
+    $title.classList.toggle("-hidden");
+    $title_input.classList.toggle("-show");
+    $title_input.focus();
+    if ($title_input.value) {
+      $title.innerHTML = $title_input.value;
+    }
+  }
+}
